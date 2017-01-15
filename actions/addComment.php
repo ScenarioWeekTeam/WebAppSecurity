b49b934001ff70ddf76dc7cc21115afb2d478d00
Alexander T Hale <thalexander@openmailbox.org>
2017-01-15 15:06:12 +0000

    Correct index url

--- actions/addComment.php
+++ actions/addComment.php
@@ -7,7 +7,7 @@ $controller = new CommentsController();
 
 $comment = $controller->addComment($_POST["name"], $_POST["email"], $_POST["phonenumber"], $_POST["course"], $_POST["comment"]);
 if ($comment === TRUE) {
-    header("Location: /index.html?success=add");
+    header("Location: /index.php?success=add");
 }
 else {
     header("Location: /feedback.php?error=add");
