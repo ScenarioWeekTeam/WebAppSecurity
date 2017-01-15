bcf77866236ea4e76e60dd49e684b5aa0e05b24a
Alexander Hale <thalexander@openmailbox.org>
2017-01-15 18:48:15 +0000

    Place exposed data in public folder

--- public/library/model.php
+++ public/library/model.php
@@ -1,7 +1,7 @@
 <?php
 
 require $root . 'library/database.php';
-require $root . 'config/config.php';
+require $root . '../config/config.php';
 
 class Model extends Database {
     protected $_model;
