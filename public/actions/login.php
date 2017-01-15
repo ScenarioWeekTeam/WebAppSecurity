f20e3706bdc8d1bd4a8a577045147185043530c4
Alexander T Hale <thalexander@openmailbox.org>
2017-01-15 19:09:57 +0000

    Check for CSRF Token

--- public/actions/login.php
+++ public/actions/login.php
@@ -6,10 +6,16 @@ require '../controllers/accountsController.php';
 
 $controller = new AccountController();
 
-$login = $controller->login(htmlspecialchars($_POST["username"]), htmlspecialchars($_POST["password"]));
+if ($_POST['CSRFTOKEN'] === $_SESSION['CSRF-Login']) {
+    $_SESSION['CSRF-Login'] = NULL;
+    $login = $controller->login(htmlspecialchars($_POST["username"]), htmlspecialchars($_POST["password"]));
 
-if ($login != -1) {
-    header("Location: /index.php?success=login");
+    if ($login != -1) {
+        header("Location: /index.php?success=login");
+    }
+    else {
+        header("Location: /login.php?error=true");
+    }
 }
 else {
     header("Location: /login.php?error=true");
