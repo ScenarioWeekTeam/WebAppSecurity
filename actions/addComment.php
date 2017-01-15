8b7f734f22c193cdce97760806cbfcf77cbbd9ee
Alexander T Hale <thalexander@openmailbox.org>
2017-01-15 18:27:18 +0000

    Escape input

--- actions/addComment.php
+++ actions/addComment.php
@@ -5,7 +5,7 @@ require '../controllers/commentsController.php';
 
 $controller = new CommentsController();
 
-$comment = $controller->addComment($_POST["name"], $_POST["email"], $_POST["phonenumber"], $_POST["course"], $_POST["comment"]);
+$comment = $controller->addComment(htmlspecialchars($_POST["name"]), htmlspecialchars($_POST["email"]), htmlspecialchars($_POST["phonenumber"]), htmlspecialchars($_POST["course"]), htmlspecialchars($_POST["comment"]));
 if ($comment === TRUE) {
     header("Location: /index.php?success=add");
 }
