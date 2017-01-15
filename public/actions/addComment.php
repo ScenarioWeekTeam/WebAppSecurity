add4232ac608451a6690f77972694ec592fd04c1
Alexander T Hale <thalexander@openmailbox.org>
2017-01-15 19:03:03 +0000

    Check for CSRF Token

--- public/actions/addComment.php
+++ public/actions/addComment.php
@@ -5,9 +5,15 @@ require '../controllers/commentsController.php';
 
 $controller = new CommentsController();
 
-$comment = $controller->addComment(htmlspecialchars($_POST["name"]), htmlspecialchars($_POST["email"]), htmlspecialchars($_POST["phonenumber"]), htmlspecialchars($_POST["course"]), htmlspecialchars($_POST["comment"]));
-if ($comment === TRUE) {
-    header("Location: /index.php?success=add");
+if ($_POST['CSRFTOKEN'] === $_SESSION['CSRF-Feedback']) {
+    $_SESSION['CSRF-Feedback'] = NULL;
+    $comment = $controller->addComment(htmlspecialchars($_POST["name"]), htmlspecialchars($_POST["email"]), htmlspecialchars($_POST["phonenumber"]), htmlspecialchars($_POST["course"]), htmlspecialchars($_POST["comment"]));
+    if ($comment === TRUE) {
+        header("Location: /index.php?success=add");
+    }
+    else {
+        header("Location: /feedback.php?error=add");
+    }
 }
 else {
     header("Location: /feedback.php?error=add");
