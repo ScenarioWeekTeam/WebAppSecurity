6b234dbf409fdc8f0ed789452bf3dfd484988f71
Alexander T Hale <thalexander@openmailbox.org>
2017-01-15 18:32:13 +0000

    Remove redirect

--- actions/login.php
+++ actions/login.php
@@ -9,12 +9,7 @@ $controller = new AccountController();
 $login = $controller->login(htmlspecialchars($_POST["username"]), htmlspecialchars($_POST["password"]));
 
 if ($login != -1) {
-    if ($_GET['redirect']) {
-        header("Location: " . $_GET['redirect']);
-    }
-    else {
-        header("Location: /index.php?success=login");
-    }
+    header("Location: /index.php?success=login");
 }
 else {
     header("Location: /login.php?error=true");
