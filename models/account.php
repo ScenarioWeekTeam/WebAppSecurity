2ece7e6acdf673754b54f224d4ecdc423542892c
Alexander T Hale <thalexander@openmailbox.org>
2017-01-15 12:02:55 +0000

    Fix syntax errors in Account model

--- models/account.php
+++ models/account.php
@@ -8,28 +8,16 @@ class Account extends Model {
         $this->initTable();
         $this->username = $username;
         $this->password = $password;
-        $this->id = $id
-        if ($this->id) {
-            if (!$this->username)
-        }
+        $this->id = $id;
     }
 
     function save() {
-        if ($this->id) {
-            if (!$this->username || !$this->password) {
-                $this->find();
-            }
-            $sql = "UPDATE " . $this->_table . " SET username=" . $this->username . " password=" . $this->password . " WHERE id=" . $this->id;
+        if ($this->username && $this->password) {
+            $sql = "INSERT INTO " . $this->_table . " (username, password) VALUES ('" . $this->username . "', '" . $this->password . "')";
             return $this->query($sql);
         }
         else {
-            if ($this->username && $this->password) {
-                $sql = "INSERT INTO " . $this->_table . " (username, password) VALUES ('" . $this->username . "', '" . $this->password . "')";
-                return $this->query($sql);
-            }
-            else {
-                return -1;
-            }
+            return -1;
         }
     }
 
@@ -43,57 +31,10 @@ class Account extends Model {
         }
     }
 
-    function find() {
-        if ($this->id) {
-            if (!$this->username && !$this->password) {
-                $sql = "SELECT username, password FROM " . $this->_table . " WHERE id=" . $this->id;
-                $result = $this->query($sql);
-            }
-            else if (!$this->username) {
-                $sql = "SELECT username FROM " . $this->_table . " WHERE id=" . $this->id;
-                $result = $this->query($sql);
-            }
-            else if (!$this->password) {
-                $sql = "SELECT password FROM " . $this->_table . " WHERE id=" . $this->id;
-                $result = $this->query($sql);
-            }
-            else {
-                return;
-            }
-        }
-        else if ($this->username && $this->password) {
-            $sql = "SELECT id FROM " . $this->_table . " WHERE username=" . $this->username . " password=" . $this->password;
-            $result = $this->query($sql);
-        }
-        else if ($this->username) {
-            $sql = "SELECT id, password FROM " . $this->_table . " WHERE username=" . $this->username;
-            $result = $this->query($sql);
-        }
-        else {
-            return -1;
-        }
-
-        if ($result->num_rows > 0) {
-            $row = $result->fetch_assoc();
-            if ($row["id"]) {
-                $this->id = $row["id"];
-            }
-            if ($row["username"]) {
-                $this->username = $row["username"];
-            }
-            if ($row["password"]) {
-                $this->password = $row["password"];
-            }
-        }
-        else {
-            return -1;
-        }
-    }
-
     function search() {
-        $sql = "SELECT id, username FROM " . $this->_table
+        $sql = "SELECT id, username FROM " . $this->_table;
         if ($this->id || $this->username || $this->password) {
-            $sql = $sql . " WHERE "
+            $sql = $sql . " WHERE ";
             if ($this->id) {
                 $sql = $sql . "id=" . $this->id . " ";
             }
